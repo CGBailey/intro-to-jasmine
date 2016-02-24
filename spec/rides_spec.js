cad4195c419007639a555e484742270840a426be
Conrad G Bailey <grhmbailey@gmail.com>
2016-02-24 13:20:00 -0700

    refactored to include two it statments

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -1,26 +1,30 @@
 var rides = require('../lib/rides');
 
   describe('Rides',function(){
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
     it('determines if a child is too short to ride a ride',function(){
-      var child1 = {
-        height: 3,
-        name: "Julian",
-        age: 3
-      };
-      var child2 = {
-        height: 4,
-        name: "Cero",
-        age: 4
-      };
-      var ride1 = {
-        minHeight: 3,
-        maxHeight: 7
-      };
-      var ride2 = {
-        minHeight: 4,
-        maxHeight: 7
-      };
       expect(rides.isTallEnough(child1,ride2)).toEqual(false)
-      expect(rides.isTallEnough(child2,ride1)).toEqual(true)
     });
+    it('determines when a child is tall enough', function() {
+      expect(rides.isTallEnough(child2,ride1)).toEqual(true)
+      expect(rides.isTallEnough(child2,ride2)).toEqual(true)
+
+});
   })
